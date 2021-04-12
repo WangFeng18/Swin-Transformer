b5cd60aa276e548fe0686e076627f142257c53fc
WangFeng18 <wangfengwork17@gmail.com>
2021-04-12 16:27:33 +0800

    init commit

---
+++ model.py
@@ -0,0 +1,189 @@
+import math
+import torch
+import torch.nn as nn
+import numpy as np
+from thop import profile
+from einops import rearrange 
+from einops.layers.torch import Rearrange, Reduce
+from layer import DropPath
+
+class WMSA(nn.Module):
+    """ Self-attention module in Swin Transformer
+    """
+
+    def __init__(self, input_dim, output_dim, head_dim, window_size):
+        super(WMSA, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.head_dim = head_dim 
+        self.n_heads = input_dim/head_dim
+        self.window_size = window_size
+        self.embedding_layer = nn.Linear(self.input_dim, 3*self.input_dim, bias=False)
+        self.relative_position_params = nn.Parameter(torch.randn(2 * window_size - 1, 2 * window_size -1))
+        self.linear = nn.Linear(self.input_dim, self.output_dim)
+
+    def generate_mask(self, w, p, shift):
+        """ generating the mask of SW-MSA
+        Args:
+            shift: shift parameters in CyclicShift.
+        Returns:
+            attn_mask: should be (1 1 w p p),
+        """
+        # supporting sqaure.
+        attn_mask = torch.zeros(w, w, p, p, p, p, dtype=torch.bool, device=self.relative_position_params.device)
+        if self.type == 'W':
+            return attn_mask
+
+        s = p - shift
+        attn_mask[-1, :, :s, :, s:, :] = True
+        attn_mask[-1, :, s:, :, :s, :] = True
+        attn_mask[:, -1, :, :s, :, s:] = True
+        attn_mask[:, -1, :, s:, :, :s] = True
+        attn_mask = rearrange(attn_mask, 'w1 w2 p1 p2 p3 p4 -> 1 1 (w1 w2) (p1 p2) (p3 p4)')
+        return attn_mask
+
+    def forward(self, x):
+        """ Forward pass of Window Multi-head Self-attention module.
+        Args:
+            x: input tensor with shape of [b h w c];
+            attn_mask: attention mask, fill -inf where the value is True; 
+        Returns:
+            output: tensor shape [b h w c]
+        """
+        if self.type!='W': x = torch.roll(x, shifts=(self.window_size//2, self.window_size//2), dims=(1,2))
+
+        x = rearrange(x, 'b (w1 p1) (w2 p2) c -> b w1 w2 p1 p2 c', p1=self.window_size, p2=self.window_size)
+        h_windows = x.size(1)
+        w_windows = x.size(2)
+        # sqaure validation
+        assert h_windows == w_windows
+
+        x = rearrange(x, 'b w1 w2 p1 p2 c -> b (w1 w2) (p1 p2) c', p1=self.window_size, p2=self.window_size)
+        qkv = self.embedding_layer(x)
+        q, k, v = rearrange(qkv, 'b nw np (threeh c) -> threeh b nw np c', c=self.head_dim).chunk(3, dim=0)
+        sim = torch.einsum('hbwpc,hbwqc->hbwpq', q, k)/math.sqrt(q.size(-1))
+
+        # Adding learnable relative embedding
+        sim = sim + self.relative_embedding()
+
+        # Using Attn Mask to distinguish different subwindows.
+        if self.type != 'W':
+            attn_mask = self.generate_mask(h_windows, self.window_size, shift=self.window_size//2)
+            sim.masked_fill_(attn_mask, float("-inf"))
+
+        probs = nn.functional.softmax(sim, dim=-1)
+        output = torch.einsum('hbwpp,hbwpc->hbwpc', probs, v)
+        output = rearrange(output, 'h b w p c -> b w p (h c)')
+        output = self.linear(output)
+        output = rearrange(output, 'b (w1 w2) (p1 p2) c -> b (w1 p1) (w2 p2) c', w1=h_windows, p1=self.window_size)
+
+        if self.type!='W': x = torch.roll(x, shifts=(-self.window_size//2, -self.window_size//2), dims=(1,2))
+        return output
+    
+    def relative_embedding(self):
+        cord = torch.tensor(np.array([[i, j] for i in range(self.window_size) for j in range(self.window_size)]))
+        relation = cord[None, :, :] - cord[:, None, :]
+        # negative is allowed
+        return self.relative_position_params[relation[:,:,0], relation[:,:,1]]
+
+class Block(nn.Module):
+    def __init__(self, input_dim, output_dim, head_dim, window_size, drop_path, type='W'):
+        """ SwinTransformer Block
+        """
+        super(Block, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        assert type in ['W', 'SW']
+        self.type = type
+
+        self.ln1 = nn.LayerNorm(input_dim)
+        self.msa = WMSA(input_dim, input_dim, head_dim, window_size)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.ln2 = nn.LayerNorm(input_dim)
+        self.mlp = nn.Sequential(
+            nn.Linear(input_dim, 4 * input_dim),
+            nn.GELU(),
+            nn.Linear(4 * input_dim, output_dim),
+        )
+
+    def forward(self, x):
+        x = x + self.drop_path(self.msa(self.ln1(x)))
+        x = x + self.drop_path(self.mlp(self.ln2(x)))
+        return x
+
+class SwinTransformer(nn.Module):
+    """ Implementation of Swin Transformer https://arxiv.org/abs/2103.14030
+    In this Implementation, the standard shape of data is (b h w c), which is a similar protocal as cnn.
+    """
+    #TODO make layers using configs
+    def __init__(self, num_classes, config=[2,2,6,2], dim=96, drop_path_rate=0.2):
+        super(SwinTransformer, self).__init__()
+        self.config = config
+        self.dim = dim
+        self.head_dim = 32
+        self.window_size = 7
+        self.patch_partition = Rearrange('b c (h1 sub_h) (w1 sub_w) -> b h1 w1 (c sub_h sub_w)', sub_h=4, sub_w=4)
+
+        # drop path rate for each layer
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(config))]
+
+        begin = 0
+        self.stage1 = [nn.Linear(48, dim),] + \
+                      [Block(dim, dim, self.head_dim, self.window_size, dpr[i+begin], 'W' if not i%2 else 'SW') 
+                      for i in range(config[0])]
+        begin += config[0]
+        self.stage2 = [Rearrange('b (h neih) (w neiw) c -> b h w (neih neiw c)', neih=2, neiw=2), nn.Linear(4*dim, 2*dim),] + \
+                      [Block(2*dim, 2*dim, self.head_dim, self.window_size, dpr[i+begin], 'W' if not i%2 else 'SW')
+                      for i in range(config[1])]
+        begin += config[1]
+        self.stage3 = [Rearrange('b (h neih) (w neiw) c -> b h w (neih neiw c)', neih=2, neiw=2), nn.Linear(8*dim, 4*dim),] + \
+                      [Block(4*dim, 4*dim, self.head_dim, self.window_size, dpr[i+begin], 'W' if not i%2 else 'SW')
+                      for i in range(config[2])]
+        begin += config[2]
+        self.stage4 = [Rearrange('b (h neih) (w neiw) c -> b h w (neih neiw c)', neih=2, neiw=2), nn.Linear(16*dim, 8*dim),] + \
+                      [Block(8*dim, 8*dim, self.head_dim, self.window_size, dpr[i+begin], 'W' if not i%2 else 'SW')
+                      for i in range(config[3])]
+        
+        self.stage1 = nn.Sequential(*self.stage1)
+        self.stage2 = nn.Sequential(*self.stage2)
+        self.stage3 = nn.Sequential(*self.stage3)
+        self.stage4 = nn.Sequential(*self.stage4)
+
+        self.mean_pool = Reduce('b h w c -> b c', reduction='mean')
+        self.classifier = nn.Sequential(
+            nn.LayerNorm(dim * 8),
+            nn.Linear(8*dim, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.patch_partition(x)
+        x = self.stage1(x)
+        x = self.stage2(x)
+        x = self.stage3(x)
+        x = self.stage4(x)
+
+        x = self.mean_pool(x)
+        x = self.classifier(x)
+        return x
+
+def Swin_T(num_classes, config=[2,2,6,2], dim=96, **kwargs):
+    return SwinTransformer(num_classes, config=config, dim=dim, **kwargs)
+
+def Swin_S(num_classes, config=[2,2,18,2], dim=96, **kwargs):
+    return SwinTransformer(num_classes, config=config, dim=dim, **kwargs)
+
+def Swin_B(num_classes, config=[2,2,18,2], dim=128, **kwargs):
+    return SwinTransformer(num_classes, config=config, dim=dim, **kwargs)
+
+def Swin_L(num_classes, config=[2,2,18,2], dim=192, **kwargs):
+    return SwinTransformer(num_classes, config=config, dim=dim, **kwargs)
+
+if __name__ == '__main__':
+    test_model = Swin_T().cuda()
+    print(test_model)
+    dummy_input = torch.rand(3,3,224,224).cuda()
+    output = test_model(dummy_input)
+    print(output.size())
+    flops, params = profile(test_model, inputs=(dummy_input, ))
+    print(params)
+    print(flops)
