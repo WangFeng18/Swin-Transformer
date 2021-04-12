631a659f2a1dfe3b839101becdd3c9c77bdc48d9
WangFeng18 <wangfengwork17@gmail.com>
2021-04-12 17:51:09 +0800

    make the val batch size same as train bs

--- train.py
+++ train.py
@@ -193,7 +193,7 @@ def main(args):
 
     data_loader_val = torch.utils.data.DataLoader(
         dataset_val, sampler=sampler_val,
-        batch_size=int(1.5 * args.batch_size),
+        batch_size=int(1.0 * args.batch_size),
         num_workers=args.num_workers,
         pin_memory=args.pin_mem,
         drop_last=False
